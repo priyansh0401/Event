46c70891150a82e9632a701eb4503213ad74a942
priyansh0401 <115654325+priyansh0401@users.noreply.github.com>
2023-07-15 16:04:48 +0530

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -69,13 +69,14 @@ signUpLink.addEventListener('click', () => {
 signInLink.addEventListener('click', () => {
     wrapper.classList.add('animate-signUp');
     wrapper.classList.remove('animate-signIn');
-}
+});
 
 
 var loginForm = document.getElementById('loginForm');
 var loginEmailInput = document.getElementById('loginEmailInput');
 var loginPasswordInput = document.getElementById('loginPasswordInput');
- loginForm.addEventListener('submit', function(event) {
+
+loginForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
   var loginEmail = loginEmailInput.value;
@@ -87,13 +88,31 @@ var loginPasswordInput = document.getElementById('loginPasswordInput');
 
   
   window.location.href = 'discord_clone.html';
-}                          
-                           
-                           
-                           
-                           
-);
+});
 
+
+var loginForm = document.getElementById('loginForm');
+var loadingOverlay = document.getElementById('loadingOverlay');
+
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  
+  loadingOverlay.classList.add('loading');
+
+  setTimeout(function() {
+    var loginEmail = loginEmailInput.value;
+    var loginPassword = loginPasswordInput.value;
+
+    alert('Login Successful!');
+    loginForm.reset();
+
+   
+    loadingOverlay.classList.remove('loading');
+
+    window.location.href = 'discord_clone.html';
+  }, 2000); // Adjust the delay time as needed
+});
 
 
 
