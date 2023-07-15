ffc4c217e84a114f972357d908e93c7467a99af7
priyansh0401 <115654325+priyansh0401@users.noreply.github.com>
2023-07-15 15:42:51 +0530

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -1,41 +1,79 @@
-var loginForm = document.getElementById('loginForm');
-var loginEmailInput = document.getElementById('loginEmailInput');
-var loginPasswordInput = document.getElementById('loginPasswordInput');
+// var loginForm = document.getElementById('loginForm');
+// var loginEmailInput = document.getElementById('loginEmailInput');
+// var loginPasswordInput = document.getElementById('loginPasswordInput');
 
-loginForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+// loginForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
 
-  var loginEmail = loginEmailInput.value;
-  var loginPassword = loginPasswordInput.value;
+//   var loginEmail = loginEmailInput.value;
+//   var loginPassword = loginPasswordInput.value;
 
  
-  alert('Login Successful!');
-  loginForm.reset();
+//   alert('Login Successful!');
+//   loginForm.reset();
 
   
-  window.location.href = 'discord_clone.html';
-});
+//   window.location.href = 'discord_clone.html';
+// });
 
 
-var loginForm = document.getElementById('loginForm');
-var loadingOverlay = document.getElementById('loadingOverlay');
+// var loginForm = document.getElementById('loginForm');
+// var loadingOverlay = document.getElementById('loadingOverlay');
 
-loginForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+// loginForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
 
   
-  loadingOverlay.classList.add('loading');
+//   loadingOverlay.classList.add('loading');
 
-  setTimeout(function() {
-    var loginEmail = loginEmailInput.value;
-    var loginPassword = loginPasswordInput.value;
+//   setTimeout(function() {
+//     var loginEmail = loginEmailInput.value;
+//     var loginPassword = loginPasswordInput.value;
 
-    alert('Login Successful!');
-    loginForm.reset();
+//     alert('Login Successful!');
+//     loginForm.reset();
 
    
-    loadingOverlay.classList.remove('loading');
+//     loadingOverlay.classList.remove('loading');
 
-    window.location.href = 'discord_clone.html';
-  }, 2000); // Adjust the delay time as needed
+//     window.location.href = 'discord_clone.html';
+//   }, 2000); // Adjust the delay time as needed
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const wrapper = document.querySelector('.wrapper');
+const signUpLink = document.querySelector('.signUp-link');
+const signInLink = document.querySelector('.signIn-link');
+
+signUpLink.addEventListener('click', () => {
+    wrapper.classList.add('animate-signIn');
+    wrapper.classList.remove('animate-signUp');
 });
+
+signInLink.addEventListener('click', () => {
+    wrapper.classList.add('animate-signUp');
+    wrapper.classList.remove('animate-signIn');
+});
+
+
+
+
+
+
+
